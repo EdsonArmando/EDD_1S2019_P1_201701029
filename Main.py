dae172467eeee022e51104c4159d7600d15c809a
EdsonArmando <guixarmando@gmail.com>
2019-08-07 12:06:03 -0700

    Report Grapviz Stack Score complete

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import random
 import time
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 from Structures.CircularDoubleList import Circular_Double_List
+from Structures.ScoreStack import Score_Stack
 cir = Circular_Double_List()
 
 stdscr = curses.initscr()
@@ -13,7 +14,6 @@ curses.cbreak()
 curses.curs_set(0)
 win.keypad(True)
 key=KEY_LEFT
-
 def moveUser():
     new = cir.returnFirst()
     while 1:
@@ -35,12 +35,18 @@ def moveUser():
         else:
             win.clear()
             menuOption()
-def foodSneak1(x,y,point):
+def foodSneak2(x,y,point):
     win.addstr(0,45, 'User: Edson')
     win.addstr(0,65, 'Points: '+ str(point))
     win.addstr(y,x, "*")
+
+def foodSneak1(x,y,point):
+    win.addstr(0,45, 'User: Edson')
+    win.addstr(0,65, 'Points: '+ str(point))
+    win.addstr(y,x, "+")
 def menuOption():
     conta=0
+    stack = Score_Stack()
     win.clear()
     stdscr.addstr(10, 10,"Hola")
     win.addstr(7,21, '1. Play')
@@ -58,27 +64,40 @@ def menuOption():
             point=0
             x=random.randint(2,120-2)
             y=random.randint(2,30-2)
+            x_x=random.randint(2,120-2)
+            y_y=random.randint(2,30-2)
             win.clear()
             win.border(0)
             key = KEY_RIGHT
             locX = 5
             locY = 5
             foodSneak1(x,y,0)
+            foodSneak2(x_x,y_y,0)
             win.addch(locY,locX,'#')
             while 1:
-                win.timeout(100)
+                win.timeout((100 - 15//3)%90)
+                if point>=15:
+                    win.timeout((100 - 20//3)%90)
                 inputKey = win.getch()
                 if inputKey is not  -1:
                     key = inputKey
 
                 win.addch(locY,locX,' ')
+
                 if key == KEY_RIGHT:
                     win.addch(locY,locX-2,' ')
                     if locX==x and locY==y:
                         point+=1
                         x=random.randint(2,120-2)
                         y=random.randint(2,30-2)
+                        stack.PushScore(x,y)
                         foodSneak1(x,y,point)
+                    elif locX==x_x and locY==y_y:
+                        point -=1
+                        x_x=random.randint(2,120-2)
+                        y_y=random.randint(2,30-2)
+                        stack.PopScore()
+                        foodSneak2(x_x,y_y,point)
                     locX = locX + 1
                 elif key == KEY_LEFT:
                     win.addch(locY,locX-2,' ')
@@ -86,7 +105,14 @@ def menuOption():
                         point+=1
                         x=random.randint(2,120-2)
                         y=random.randint(2,30-2)
+                        stack.PushScore(x,y)
                         foodSneak1(x,y,point)
+                    elif locX==x_x and locY==y_y:
+                        point -=1
+                        x_x=random.randint(2,120-2)
+                        y_y=random.randint(2,30-2)
+                        stack.PopScore()
+                        foodSneak2(x_x,y_y,point)
                     locX = locX - 1
                 elif key == KEY_UP:
                     win.addch(locY,locX-3,' ')
@@ -96,7 +122,14 @@ def menuOption():
                         point+=1
                         x=random.randint(2,120-2)
                         y=random.randint(2,30-2)
+                        stack.PushScore(x,y)
                         foodSneak1(x,y,point)
+                    elif locX==x_x and locY==y_y:
+                        point -=1
+                        x_x=random.randint(2,120-2)
+                        y_y=random.randint(2,30-2)
+                        stack.PopScore()
+                        foodSneak2(x_x,y_y,point)
                     locY = locY - 1
                 elif key == KEY_DOWN:
                     win.addch(locY,locX-3,' ')
@@ -106,19 +139,30 @@ def menuOption():
                         point+=1
                         x=random.randint(2,120-2)
                         y=random.randint(2,30-2)
+                        stack.PushScore(x,y)
                         foodSneak1(x,y,point)
+                    elif locX==x_x and locY==y_y:
+                        point -=1
+                        x_x=random.randint(2,120-2)
+                        y_y=random.randint(2,30-2)
+                        stack.PopScore()
+                        foodSneak2(x_x,y_y,point)
                     locY = locY + 1
+                #elif point=>15:
+                    #win.timeout(100 - (len(snake)//3)%90)
                 elif key==27:
                     win.clear()
                 elif key==53:
                     if conta==0:
-                        print("Edson")
+                        stack.printStack()
+                        stack.generearDoc()
                         conta+=1
 
 
                 win.addch(locY,locX,'#')
                 win.addch(locY,locX-1,'#')
                 win.addch(locY,locX-2,'#')
+
 
 
 
--- Structures/ScoreStack.py
+++ Structures/ScoreStack.py
@@ -2,11 +2,12 @@ class NodeScore:
     def __init__(self,x,y):
         self.x=x
         self.y=y
+
+        self.cont=0
         self.next=None
 class Score_Stack:
     def __init__(self):
         self.first=None
-
     def PushScore(self,x,y):
         new = NodeScore(x,y)
         if self.first is None:
@@ -14,20 +15,38 @@ class Score_Stack:
         else:
             new.next=self.first
             self.first=new
+    def printStack(self):
+        aux = self.first
+        while aux !=None:
+            print(aux.x,aux.y)
+            aux = aux.next
+
+    def generearDoc(self):
+        texto = "digraph {\n rankdir=LR; \n node [shape=record];"
+        pila1 = 'subgraph cluster_0 { \n  label="Pila Scores 1"; \n struct1 [label ="|'
+        datosPila1=""
+        cont = 0
+        aux = self.first
+        while aux !=None:
+            datosPila1 += "<f"
+            datosPila1 += str(cont)
+            datosPila1 += ">"
+            datosPila1 += str(aux.x)+","+str(aux.y)
+            datosPila1 += "|"
+            cont+=1
+            aux = aux.next
+        if aux is None:
+            if self.first is None:
+                datosPila1="<f0>"
+            datosPila1=datosPila1[:-1]
+            print(datosPila1)
+            datosPila1 =datosPila1 + '"];\n}\n}'
+            with open("Pila1.txt",'w',encoding = 'utf-8') as f:
+                f.write(texto+pila1+datosPila1)
+
     def PopScore(self):
         x=0
         y=0
         x = self.first.x
         y = self.first.y
         self.first=self.first.next
-    def printStack(self):
-        aux = self.first
-        while aux !=None:
-            print(aux.x,aux.y)
-            aux = aux.next
-stack1=Score_Stack()
-stack1.PushScore(4,5)
-stack1.PushScore(7,3)
-stack1.PushScore(2,5)
-stack1.PopScore()
-stack1.printStack()
